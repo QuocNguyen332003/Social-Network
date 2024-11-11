fa75d2ed0288ff859b1d7cb7ef7d7cf2b81112ee
MquanImart <phanminhquanmh@gmail.com>
2024-11-11 10:03:15 +0700

    sibar

---
+++ src/interface/contactsInterface.ts
@@ -0,0 +1,8 @@
+import { MyPhoto } from "./interface";
+
+export interface Contacts{
+    _id: string;
+    avt: MyPhoto,
+    name: string,
+    status: string,
+}
--- src/shared/components/sidebarRight/SidebarRight.tsx
+++ src/shared/components/sidebarRight/SidebarRight.tsx
@@ -1,6 +1,9 @@
 import { List, ListItem, ListItemText, Avatar, Box, Typography, ListItemButton } from '@mui/material';
 import { styled } from '@mui/system';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { Contacts } from '../../../interface/contactsInterface';
 
 const StyledListItem = styled(ListItem)({
   position: 'relative',
@@ -21,33 +24,36 @@ const StyledListItem = styled(ListItem)({
 
 const SidebarRight = () => {
   const navigate = useNavigate();
-  const contacts = [
-    {
-      userID: "u123",
-      avt: "/src/assets/data-test/avt1.png",
-      name: "John Doe",
-    },
-    {
-      userID: "u456",
-      avt: "/src/assets/data-test/avt2.jpg",
-      name: "Jane Smith",
-    },
-    {
-      userID: "u789",
-      avt: "/src/assets/data-test/avt3.jpg",
-      name: "Emily Johnson",
-    },
-    {
-      userID: "u321",
-      avt: "/src/assets/data-test/avt4.jpeg",
-      name: "Michael Brown",
-    },
-    {
-      userID: "u654",
-      avt: "/src/assets/data-test/avt1.png",
-      name: "Sophia Davis",
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+  const token = sessionStorage.getItem('token');
+  const currentUserId = sessionStorage.getItem('userId') || '';
+
+  const [contacts, setContacts] = useState<Contacts[]>([]);
+
+  useEffect(()=> {
+    getDataFriend();
+  }, [])
+
+  const getDataFriend = async () => {
+    try {
+      const response = await axios.get(`http://localhost:3000/v1/user/friends/${currentUserId}`, 
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      setContacts(response.data);
+    } catch (error) {
+      console.error('Lỗi khi lấy bài viết:', error);
+      setError('Lỗi khi tải bài viết. Vui lòng thử lại sau.');
+    } finally {
+      setIsLoading(false); 
     }
-  ];
+  }
+  if (contacts.length <= 0) return <></>
   return (
     <Box sx={{ padding: '16px', borderLeft: '1px solid #e0e0e0', height: '85vh' }}>
       <Typography variant="h6" gutterBottom sx={{ fontWeight: 'bold' }}>
@@ -56,8 +62,8 @@ const SidebarRight = () => {
       <List>
         {contacts.map((contact, index) => (
           <ListItemButton key={index} sx={{ mb: 1 }}
-            onClick={() => navigate(`/messages?userIDStart=${contact.userID}`)}>
-            <Avatar alt={contact.name} src={contact.avt} />
+            onClick={() => navigate(`/messages?userIDStart=${contact._id}`)}>
+            <Avatar alt={contact.name} src={contact.avt.link} />
             <ListItemText primary={contact.name} sx={{ marginLeft: '16px' }} />
           </ListItemButton>
         ))}
